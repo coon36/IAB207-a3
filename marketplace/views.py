d741256234dde1a24e2843d688ca9c10be481d72
Brianna <briannakhassett@live.com.au>
2019-10-02 17:25:53 +1000

    ---

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,13 +1,14 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
-
+from .models import Listing, Bid, User, Transaction
+from . import db
 
 bp = Blueprint('main', __name__)
 
-
 @bp.route('/')
 def index():
-    return render_template('Homepage.html')
+    listing = Listing.query.filter_by(date=date_posted).all()
+    return render_template('Homepage.html', listing=listing)
 
 @bp.route('/manageitems')
 # @login_required
@@ -17,10 +18,6 @@ def manage():
 @bp.route('/results')
 def result():
     return render_template('result.html')
-
-@bp.route('/viewlisting')
-def viewlisting():
-    return render_template('ViewListing.html')
 
 @bp.route('/sellerhistory')
 # @login_required
