3a30fc369391c8117418722a1080c5aec5253b12
Brianna <briannakhassett@live.com.au>
2019-10-02 16:04:20 +1000

    Testing

--- marketplace/forms.py
+++ marketplace/forms.py
@@ -5,17 +5,23 @@ from wtforms.validators import InputRequired, Length, Email, EqualTo
 
 #login form
 class LoginForm(FlaskForm):
-    user_name=StringField("Username", validators=[InputRequired('Please enter your username.')])
-    password=PasswordField("Password", validators=[InputRequired('Please enter your password.')])
+    user_name = StringField("Username", validators=[InputRequired('Please enter your username.')])
+    password = PasswordField("Password", validators=[InputRequired('Please enter your password.')])
     submit = SubmitField("Login")
 
 #registration form
 class RegisterForm(FlaskForm):
-    user_name=StringField("Username", validators=[InputRequired('Please enter a username.')])
+    user_name = StringField("Username", validators=[InputRequired('Please enter a username.')])
     email_id = StringField("Email Address", validators=[Email("Please enter a valid email address.")])
     #linking two fields - password should be equal to data entered in confirm
-    password=PasswordField("Password", validators=[InputRequired('Please enter a password.'),
+    password = PasswordField("Password", validators=[InputRequired('Please enter a password.'),
                   EqualTo('confirm', message="Passwords should match.")])
     confirm = PasswordField("Please confirm your password.")
     #submit button
     submit = SubmitField("Register")
+
+# for jared, whenever
+#create listing form
+#class CreateListingForm(FlaskForm):
+#    listing_title = StringField("Listing Title:", validators=[InputRequired('Please enter a title.')])
+#    purchase_price =
