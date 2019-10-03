b548eb012102b49b791dd1f20d932ccb5a5662a2
tacticalblanky <55231111+tacticalblanky@users.noreply.github.com>
2019-10-03 16:20:27 +1000

    itemcreationform update
    drop down menus with options added and datefield modified.

--- marketplace/forms.py
+++ marketplace/forms.py
@@ -1,9 +1,10 @@
 
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
-from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField, IntegerField, DateField, FloatField
+from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField, IntegerField, FloatField
 from wtforms.validators import InputRequired, Length, Email, EqualTo, DataRequired
-from wtforms import RadioField, StringField, SubmitField
+from wtforms import RadioField, StringField, SubmitField, SelectField
+from wtforms.fields.html5 import DateField
 
 #login form
 class LoginForm(FlaskForm):
@@ -23,11 +24,7 @@ class RegisterForm(FlaskForm):
     submit = SubmitField("Register")
 
 
-# for jared, whenever
-#create listing form
-#class CreateListingForm(FlaskForm):
-#    listing_title = StringField("Listing Title:", validators=[InputRequired('Please enter a title.')])
-#    purchase_price =
+
 
 
 ALLOWED_FILE = {'png', 'jpg', 'JPG', 'PNG', 'bmp', 'JPEG'}
@@ -38,9 +35,15 @@ class ItemCreationForm(FlaskForm):
     description = TextAreaField('Description', validators=[InputRequired(), Length(min=10, max=200)])
     game_condition = RadioField('Condition Of Game', choices=[('value','New'),('value_two','Preowned')])
     game_release_date = DateField('Release Date', validators=[InputRequired()], format='%Y-%m-%d')
-    game_genre = StringField('Genre', validators=[InputRequired()])
-    game_classification = StringField('Classification', validators=[InputRequired(),])
-    game_platform = StringField('Platform', validators=[InputRequired()])
+    game_genre = SelectField('Genre', choices=[('Action', 'Action'), ('Adventure', 'Adventure',), ('Casual', 'Casual'), 
+    ('Party', 'Party'), ('Role-Playing', 'Role-Playing'), ('Simulation', 'Simulation'), 
+    ('Strategy', 'Strategy'), ('Sports', 'Sports'), ('MMO', 'MMO')])
+    game_classification = SelectField('Classification', choices=[('G', '(G)'), ('PG', '(PG)'), ('M', '(M)'), ('MA', '(MA15+)'), ('R18', '(R18+)')])
+    game_platform = SelectField('Platform', validators=[InputRequired()], choices=[('Arcade', 'Arcade'), ('Atari', 'Atari'), 
+    ('Commodore 64', 'Commodore 64'), ('Nintendo', 'Nintendo'), ('Nintendo 3DS', 'Nintendo 3DS'), ('Nintendo DS', 'Nintendo DS'),  
+    ('Nintendo Switch', 'Nintendo Switch'), ('Nintendo Wii', 'Nintendo Wii'), ('Nintendo Wii U', 'Nintendo Wii U'),  ('PC', 'PC'),
+    ('Playstation 3', 'Playstation 3'), ('Playstation Classic', 'Playstation Classic'), ('Playstation Vita', 'Playstation Vita'),
+    ('SEGA', 'SEGA'), ('Xbox 360', 'Xbox 360'), ('Xbox One', 'Xbox One')])
     listing_img_url = FileField('Upload Image', validators=[FileRequired(message='Image can not be empty'),
                                             FileAllowed(ALLOWED_FILE, message='Only supports png, jpg, JPG, PNG, bmp, JPEG')])
     submit = SubmitField("Create Listing")
