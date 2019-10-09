1e22df5ddac51046a97c82f9b541033d5e82e7eb
JasonThai13 <55230968+JasonThai13@users.noreply.github.com>
2019-10-09 21:35:43 +1000

    changes to result html

--- marketplace/result.py
+++ marketplace/result.py
@@ -1,21 +1,20 @@
-from flask import ( 
-    Blueprint, flash, render_template, request, url_for, redirect
-) 
+from flask import Blueprint, render_template
 from .models import Listing
 from . import db
 
 
 bp = Blueprint('result', __name__, url_prefix='/results')
 
-@bp.route('/<id>') 
-def result(id): 
-  listing = Listing.query.filter_by(id=id)
-  return render_template('result.html', listing=listing)
+# @bp.route('/<id>') 
+# def result(id): 
+#     listing = Listing.query.filter_by(id=id)
+#     return render_template('result.html', listing=listing)
 
-# @bp.route('/<game_genre>') 
-# def result(game_genre): 
-#   listing = Listing.query.filter_by(game_genre=game_genre).all() 
-#   return render_template('result.html', listing=listing)
+
+@bp.route('/<game_genre>') 
+def result(game_genre): 
+  listing = Listing.query.filter_by(game_genre=game_genre).all() 
+  return render_template('result.html', listing=listing)
 
 # @bp.route('/<game_platform>') 
 # def result(game_platform): 
