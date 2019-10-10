9b43f791cda4029250c0393346c1b15bfb89f2bf
JasonThai13 <55230968+JasonThai13@users.noreply.github.com>
2019-10-10 15:20:05 +1000

    Update result.py

--- marketplace/result.py
+++ marketplace/result.py
@@ -8,16 +8,25 @@ bp = Blueprint('result', __name__, url_prefix='/results')
 # @bp.route('/<id>') 
 # def result(id): 
 #     listing = Listing.query.filter_by(id=id)
-#     return render_template('result.html', listing=listing)
+#     return render_template('Result.html', listing=listing)
 
 
 @bp.route('/<game_genre>') 
-def result(game_genre): 
-  listing = Listing.query.filter_by(game_genre=game_genre).all() 
-  return render_template('result.html', listing=listing)
+def show(game_genre): 
+    listing = Listing.query.filter_by(game_genre=game_genre).all() 
+    return render_template('Result.html', listing=listing)
 
 # @bp.route('/<game_platform>') 
 # def result(game_platform): 
 #   listing = Listing.query.filter_by(game_platform=game_platform).all() 
-#   return render_template('result.html', listing=listing)
+#   return render_template('Result.html', listing=listing)
 
+# @bp.route('/<game_condition>') 
+# def result(game_condition): 
+#   listing = Listing.query.filter_by(game_condition=game_condition).all() 
+#   return render_template('Result.html', listing=listing)
+
+# @bp.route('/<game_classification>') 
+# def result(game_classification): 
+#   listing = Listing.query.filter_by(game_classification=game_classification).all() 
+#   return render_template('Result.html', listing=listing)
