bd9142bc94b7c022ab5f025392bf0f0160fc09b2
coon36 <28662880+coon36@users.noreply.github.com>
2019-10-10 18:26:04 +1000

    Enter listing management through each listing's page now. Still need to ensure users can only manage listings they've created.

--- marketplace/listings.py
+++ marketplace/listings.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, render_template, abort
-from flask_login import login_required
 from .models import Listing, Bid, User, Transaction
 from . import db
 
@@ -8,9 +7,5 @@ bp = Blueprint('listings', __name__, url_prefix='/listings')
 
 @bp.route('/<id>')
 def show(id):
-    listing = Listing.query.filter_by(id=id).first()
-    if listing is None:
-        abort(404)
-        return render_template('404.html')
-    else:
-        return render_template('ViewListing.html', listing=listing)
+    listing = Listing.query.filter_by(id=id).first_or_404()
+    return render_template('ViewListing.html', listing=listing)
--- marketplace/manage.py
+++ marketplace/manage.py
@@ -9,9 +9,5 @@ bp = Blueprint('manage', __name__, url_prefix='/manage')
 @bp.route('/<id>')
 @login_required
 def manage(id):
-    listing = Listing.query.filter_by(id=id).first()
-    if listing is None:
-        abort(404)
-        return render_template('404.html')
-    else:
-        return render_template('ManageListing.html', listing=listing)
+    listing = Listing.query.filter_by(id=id).first_or_404()
+    return render_template('ManageListing.html', listing=listing)
