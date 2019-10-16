d5de1e8f37b82035dec654daf4689f642d61d5ed
JasonThai13 <55230968+JasonThai13@users.noreply.github.com>
2019-10-16 14:11:26 +1000

    Fixed CSS for result page

--- marketplace/result.py
+++ marketplace/result.py
@@ -13,23 +13,23 @@ from sqlalchemy import desc
 
 bp = Blueprint('result', __name__, url_prefix='/results')
 
-@bp.route('/condition/<game_condition>') 
+@bp.route('/condition_id=<game_condition>') 
 def condition(game_condition): 
   listings = Listing.query.filter_by(game_condition=game_condition).all() 
   return render_template('Result.html', listings=listings)
 
-@bp.route('/classification/<game_classification>') 
+@bp.route('/classification_id=<game_classification>') 
 def classification(game_classification): 
     listings = Listing.query.filter_by(game_classification=game_classification).all()
     return render_template('Result.html', listings=listings)
 
-@bp.route('/platform/<game_platform>') 
+@bp.route('/platform_id=<game_platform>') 
 def platform(game_platform): 
     print(game_platform)
     listings = Listing.query.filter_by(game_platform=game_platform).all() 
     return render_template('Result.html', listings=listings)
 
-@bp.route('/genre/<game_genre>') 
+@bp.route('/genre_id=<game_genre>') 
 def genre(game_genre): 
     listings = Listing.query.filter_by(game_genre=game_genre).all() 
     return render_template('Result.html', listings=listings)
