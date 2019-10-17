762c63428edaaed8f4f2b49a6d7c3dbaf5b2506a
coon36 <28662880+coon36@users.noreply.github.com>
2019-10-17 14:03:40 +1000

    Neatening tweaks for listing management code.

--- marketplace/manage.py
+++ marketplace/manage.py
@@ -11,6 +11,5 @@ bp = Blueprint('manage', __name__, url_prefix='/manage')
 def manage(id):
     listing = Listing.query.filter_by(id=id).first_or_404()
     bids = Bid.query.filter_by(listing_id=id).join(User, Bid.user_id==User.id).\
-    add_columns(User.user_name, Bid.contact_number, Bid.date_of_bid).all()
-    users = User.query.all()
-    return render_template('ManageListing.html', listing=listing, bids=bids, user=users)
+    add_columns(User.id, User.user_name, Bid.listing_id, Bid.contact_number, Bid.date_of_bid).all()
+    return render_template('ManageListing.html', listing=listing, bids=bids)
