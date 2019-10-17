7f60362a0277a8f9a676cb5205605904a9797be7
Brianna <briannakhassett@live.com.au>
2019-10-17 22:21:41 +1000

    Select bid now saves data to the database from a query

--- marketplace/manage.py
+++ marketplace/manage.py
@@ -1,4 +1,3 @@
-from flask import Blueprint, render_template, abort, request
 from flask_login import login_required
 from .models import Listing, Bid, User, Transaction
 from . import db
@@ -7,7 +6,16 @@ from datetime import datetime, date
 bp = Blueprint('manage', __name__, url_prefix='/manage')
 
 
-@bp.route('/<id>')
+# @bp.route('/<id>')
+# @login_required
+# def manage(id):
+#     listing = Listing.query.filter_by(id=id).first_or_404()
+#     bids = Bid.query.filter_by(listing_id=id).join(User, Bid.user_id==User.id).\
+#     add_columns(User.id, User.user_name, Bid.listing_id, Bid.contact_number,
+#     Bid.date_of_bid).all()
+#     return render_template('ManageListing.html', listing=listing, bids=bids)
+
+@bp.route('/<id>', methods=['GET', 'POST'])
 @login_required
 def manage(id):
     listing = Listing.query.filter_by(id=id).first_or_404()
@@ -20,4 +28,8 @@ def manage(id):
     Transaction.user_id==User.id).add_columns(User.id, User.user_name,
     Transaction.id, Transaction.purchase_date).first()
 
-    return render_template('ManageListing.html', listing=listing, bids=bids, purchase=purchase)
+    selected = Transaction(purchase_date = date.today(), user_id = request.args.get('user_id'), listing_id = request.args.get('listing_id'))
+    db.session.add(selected)
+    db.session.commit()
+
+    return render_template('ManageListing.html', listing=listing, bids=bids, purchase=purchase, selected=selected)
