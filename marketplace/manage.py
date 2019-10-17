8a0450f39d6f6d5f8e32f724031b17cbf883d1eb
coon36 <28662880+coon36@users.noreply.github.com>
2019-10-17 13:54:40 +1000

    Bid information now shows completely correctly in listing mgmt.

--- marketplace/manage.py
+++ marketplace/manage.py
@@ -10,5 +10,7 @@ bp = Blueprint('manage', __name__, url_prefix='/manage')
 @login_required
 def manage(id):
     listing = Listing.query.filter_by(id=id).first_or_404()
-    bids = Bid.query.filter_by(listing_id=id).all()
-    return render_template('ManageListing.html', listing=listing, bids=bids)
+    bids = Bid.query.filter_by(listing_id=id).join(User, Bid.user_id==User.id).\
+    add_columns(User.user_name, Bid.contact_number, Bid.date_of_bid).all()
+    users = User.query.all()
+    return render_template('ManageListing.html', listing=listing, bids=bids, user=users)
