0381c942c6e76668ccfcb183192ec204845fda14
Brianna <briannakhassett@live.com.au>
2019-10-23 18:35:51 +1000

    abc

--- marketplace/result.py
+++ marketplace/result.py
@@ -8,7 +8,7 @@ bp = Blueprint('result', __name__, url_prefix='/results')
 @bp.route('/condition_id=<game_condition>') 
 def condition(game_condition):
   listings = Listing.query.filter_by(game_condition=game_condition).all()
-  return render_template('result.html', listings=listings, heading=heading)
+  return render_template('result.html', listings=listings)
 
 @bp.route('/classification_id=<game_classification>') 
 def classification(game_classification):
